698eb237a9c0280907baa54d891af471fc6bf045
Danny-Robinson <dannyrobinson94@hotmail.com>
2022-12-02 17:21:14 +0000

    test-image

--- constants/staticProps.ts
+++ constants/staticProps.ts
@@ -2,7 +2,7 @@ import { PageMeta } from '../types/next';
 
 export const GENERIC_TITLE = 'Green Aid Investment Fund';
 export const GENERIC_DESCRIPTION = 'Generic description here TODO';
-export const GENERIC_IMAGE = 'Generic image here TODO';
+export const GENERIC_IMAGE = '/img/testing.png'; //TODO
 export const PAGE_META_BASE: Pick<PageMeta, 'domain' | 'type'> = {
 	domain: '',
 	type: 'website'
