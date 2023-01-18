8ebd89c4821e4db6ef27995ce04a60088b945135
Danny-Robinson <dannyrobinson94@hotmail.com>
2023-01-18 12:44:47 +0000

    viewport

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -58,10 +58,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1"
-        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
       <HeaderTags meta={pageProps?.meta} />
