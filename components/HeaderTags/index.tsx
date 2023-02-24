606370d2c0f2003cfd37daa8201632e9b02fe714
Danny-Robinson <dannyrobinson94@hotmail.com>
2023-02-24 12:02:23 +0200

    more-meta

--- components/HeaderTags/index.tsx
+++ components/HeaderTags/index.tsx
@@ -19,32 +19,57 @@ export const HeaderTags = ({ meta }: HeaderTagsProps) => {
 
       <meta name="description" content={description} />
 
-      <meta property="og:title" content={title} />
+      {/* <meta property="og:title" content={title} />
       <meta property="og:url" content={url} />
       <meta property="og:type" content={type} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={`${domain}${image}`} />
+      <meta name="image" property="og:image" content={`${domain}${image}`} />
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta property="twitter:domain" content={domain} />
       <meta property="twitter:url" content={url} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={`${domain}${image}`} />
+      <meta name="twitter:image" content={`${domain}${image}`} /> */}
 
-      <link rel="icon" type="image/png" sizes="32x32" href={image} />
-      <link rel="icon" type="image/png" sizes="16x16" href={image} />
+      <meta property="og:url" content="https://www.greenaid.com" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Green Aid Investment Fund" />
+      <meta
+        property="og:description"
+        content="Green Aid is an investment fund focused on accelerating private sector investments into high-impact nature-based solutions globally."
+      />
+      <meta
+        property="og:image"
+        content="https://www.greenaid.com/img/splash.png"
+      />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="greenaid.com" />
+      <meta property="twitter:url" content="https://www.greenaid.com" />
+      <meta name="twitter:title" content="Green Aid Investment Fund" />
+      <meta
+        name="twitter:description"
+        content="Green Aid is an investment fund focused on accelerating private sector investments into high-impact nature-based solutions globally."
+      />
+      <meta
+        name="twitter:image"
+        content="https://www.greenaid.com/img/splash.png"
+      />
+
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon.ico" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon.ico" />
       <link
         rel="apple-touch-icon"
         type="image/png"
         sizes="167x167"
-        href={image}
+        href="/favicon.ico"
       />
       <link
         rel="apple-touch-icon"
         type="image/png"
         sizes="180x180"
-        href={image}
+        href="/favicon.ico"
       />
     </Head>
   );
